6d96b0768642e6fe2ae819d8a04ecb24546f41a2
Eric Alves <eralves01@hotmail.com>
2022-07-01 16:01:57 -0300

    Filtrando resultados da consulta com WHERE

--- laravel/app/Http/Controllers/MarcaController.php
+++ laravel/app/Http/Controllers/MarcaController.php
@@ -17,9 +17,33 @@ class MarcaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json($this->marca->with("modelos")->get(), 200);
+        $marcas = array();
+
+        if ($request->has("atributos_modelos")) {
+            $atributos_modelos = $request->atributos_modelos;
+            $marcas = $this->marca->with('modelos:marca_id,'.$atributos_modelos);
+        }else {
+            $marcas = $this->marca->with("modelos");
+        }
+
+        if ($request->has("filtro")) {
+            $filtros = explode(";", $request->filtro);
+            foreach ($filtros as $key => $condicao) {
+                $c = explode(":", $condicao);
+                $marcas = $marcas->where($c[0], $c[1], $c[2]);
+            }
+        }
+
+        if ($request->has("atributos")) {
+            $atributos = $request->atributos;
+            $marcas = $marcas->selectRaw($atributos)->get();
+        } else {
+            $marcas = $marcas->get();
+        }
+
+        return response()->json($marcas, 200);
     }
 
     /**
