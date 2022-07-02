067951bb2f0c8b6da74b982ddb8fcd397e9a9b99
Eric Alves <eralves01@hotmail.com>
2022-07-02 02:05:54 -0300

    Implementando REPOSITORY Design Pattern

--- laravel/app/Http/Controllers/MarcaController.php
+++ laravel/app/Http/Controllers/MarcaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
 use App\Models\Marca;
+use App\Repositories\MarcaRepository;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
@@ -19,31 +20,24 @@ class MarcaController extends Controller
      */
     public function index(Request $request)
     {
-        $marcas = array();
+        $marcaRepository = new MarcaRepository($this->marca);
 
         if ($request->has("atributos_modelos")) {
-            $atributos_modelos = $request->atributos_modelos;
-            $marcas = $this->marca->with('modelos:marca_id,'.$atributos_modelos);
+            $atributos_modelos = 'modelos:marca_id,'.$request->atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
         }else {
-            $marcas = $this->marca->with("modelos");
+            $marcaRepository->selectAtributosRegistrosRelacionados("modelos");
         }
 
         if ($request->has("filtro")) {
-            $filtros = explode(";", $request->filtro);
-            foreach ($filtros as $key => $condicao) {
-                $c = explode(":", $condicao);
-                $marcas = $marcas->where($c[0], $c[1], $c[2]);
-            }
+            $marcaRepository->filtros($request->filtro);
         }
 
         if ($request->has("atributos")) {
-            $atributos = $request->atributos;
-            $marcas = $marcas->selectRaw($atributos)->get();
-        } else {
-            $marcas = $marcas->get();
+            $marcaRepository->selectAtributos($$request->atributos);
         }
 
-        return response()->json($marcas, 200);
+        return response()->json($marcaRepository->getResultado(), 200);
     }
 
     /**
--- laravel/app/Http/Controllers/ModeloController.php
+++ laravel/app/Http/Controllers/ModeloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
 use App\Models\Modelo;
+use App\Repositories\ModeloRepository;
 use Illuminate\Http\Request;
 
 class ModeloController extends Controller
@@ -18,31 +19,24 @@ class ModeloController extends Controller
      */
     public function index(Request $request)
     {
-        $modelos = array();
+        $modeloRepository = new ModeloRepository($this->modelo);
 
         if ($request->has("atributos_marca")) {
-            $atributos_marca = $request->atributos_marca;
-            $modelos = $this->modelo->with('marca:id,'.$atributos_marca);
+            $atributos_marca = 'marca:id,'.$request->atributos_marca;
+            $modeloRepository->selectAtributosRegistrosRelacionados($atributos_marca);
         }else {
-            $modelos = $this->modelo->with("marca");
+            $modeloRepository->selectAtributosRegistrosRelacionados("marca");
         }
 
         if ($request->has("filtro")) {
-            $filtros = explode(";", $request->filtro);
-            foreach ($filtros as $key => $condicao) {
-                $c = explode(":", $condicao);
-                $modelos = $modelos->where($c[0], $c[1], $c[2]);
-            }
+            $modeloRepository->filtros($request->filtro);
         }
 
         if ($request->has("atributos")) {
-            $atributos = $request->atributos;
-            $modelos = $modelos->selectRaw($atributos)->get();
-        } else {
-            $modelos = $modelos->get();
+            $modeloRepository->selectAtributos($request->atributos);
         }
 
-        return response()->json($modelos, 200);
+        return response()->json($modeloRepository->getResultado(), 200);
     }
 
     /**
